4b2d5288231ec38b8c65f6490b8b0807eff9f157
blackchestnut <alexander.kalinichev@gmail.com>
2014-11-09 16:25:41 +0300

    Init rspec

--- Gemfile
+++ Gemfile
@@ -38,16 +38,3 @@ group :test, :development do
   gem 'shoulda'
   gem 'shoulda-matchers'
 end
-
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
-# Use debugger
-# gem 'debugger', group: [:development, :test]
-
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,15 @@ module Agileseason
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.helper false
+      g.helper_specs false
+      g.stylesheets false
+      g.template_engine :slim
+      g.test_framework :rspec
+      g.view_specs false
+    end
   end
 end
