99aa8c6e4d5dde0a86874a33526342eb9352881a
blackchestnut <alexander.kalinichev@gmail.com>
2014-11-13 08:26:51 +0300

    #5 Add Boards controller

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,4 @@
+class BoardsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :repos, only: [:index]
+  resources :boards, only: [:new]
 
   get '/auth/github/callback', to: 'sessions#create'
   get '/sign_out', to: 'sessions#destroy'
