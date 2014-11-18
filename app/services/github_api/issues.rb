4ef7368f59ef5c71577c927fa1900c11b89643f6
blackchestnut <alexander.kalinichev@gmail.com>
2014-11-18 08:52:05 +0300

    #8 Add hidden comment to issue body

--- app/services/github_api/issues.rb
+++ app/services/github_api/issues.rb
@@ -12,7 +12,24 @@ class GithubApi
     end
 
     def create_issue(board, issue)
-      client.create_issue(board.github_id, issue.title, issue.body, { labels: board.columns.first.label_name })
+      column = board.columns.first
+      full_body = issue.body + hidden_track(column)
+      client.create_issue(board.github_id, issue.title, full_body, { labels: column.label_name })
+    end
+
+  private
+    def hidden_track(column)
+      hidden_content({
+        track_stats: {
+          column_id: column.id,
+          in_at: Time.current.to_s,
+          out_at: nil,
+        }
+      })
+    end
+
+    def hidden_content(hash)
+      "\n<!---\n@agileseason:#{hash}\n-->"
     end
   end
 end
