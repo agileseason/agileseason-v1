c2a6de501cec3cc566a2afa9c64ea0890801b08c
blackchestnut <alexander.kalinichev@gmail.com>
2014-11-18 07:28:35 +0300

    Show issues on baords#show

---
+++ app/services/github_api/issues.rb
@@ -0,0 +1,14 @@
+class GithubApi
+  module Issues
+    def board_issues(board)
+      labels = board.columns.map(&:label_name)
+      issues = client.issues(board.github_id)
+      board_labels = labels.inject({}) { |mem, e| mem[e] = []; mem }
+      issues.each do |issue|
+        label = issue.labels.find { |e| labels.include?(e.name) }
+        board_labels[label.name] << issue if label
+      end
+      board_labels
+    end
+  end
+end
--- app/services/github_api/labels.rb
+++ app/services/github_api/labels.rb
@@ -1,11 +1,10 @@
 class GithubApi
   module Labels
     def sync_labels(board)
-      repo_id = repo(board.github_id).id
-      labels = client.labels(repo_id).map(&:name)
+      labels = client.labels(board.github_id).map(&:name)
       missing_columns = board.columns.select { |column| column unless labels.include?(column.label_name) }
       missing_columns.each do |column|
-        client.add_label(repo_id, column.label_name, column.color)
+        client.add_label(board.github_id, column.label_name, column.color)
       end
     end
   end
