c1f916dcb9587b22e02e00d168ecece1f2d5ac23
blackchestnut <alexander.kalinichev@gmail.com>
2014-12-01 23:17:56 +0300

    #12 Refactor and fix TrackStats

--- app/services/github_api/issues.rb
+++ app/services/github_api/issues.rb
@@ -13,7 +13,7 @@ class GithubApi
 
     def create_issue(board, issue)
       column = board.columns.first
-      full_body = issue.body + hidden_track(column)
+      full_body = issue.body + TrackStats.track(column.id)
       client.create_issue(board.github_id, issue.title, full_body, { labels: column.label_name })
     end
 
@@ -25,35 +25,9 @@ class GithubApi
       issue = client.issue(board.github_id, number)
       /(.*)\n<!---\s@agileseason:(.*)\s-->(.*)/im =~ issue.body
       hash = eval($2)
-# track all - set out_at
-      hash[:track_stats][:columns].each do |key, value|
-        value[:out_at] = Time.current.to_s
-      end
-# track current in
-      hash[:track_stats][:columns][column.id] = { in_at: Time.current.to_s, out_at: nil }
-
       labels = issue.labels.map(&:name) - board.columns.map(&:label_name) << column.label_name
-      body = "#{$1}#{hidden_content(hash)}#{$3}"
+      body = "#{$1}#{TrackStats.track(column.id, hash)}#{$3}"
       client.update_issue(board.github_id, number, issue.title, body, { labels: labels })
-    end
-
-    private
-
-    def hidden_track(column)
-      hidden_content({
-        track_stats: {
-          columns: {
-            column.id => {
-              in_at: Time.current.to_s,
-              out_at: nil
-            }
-          }
-        }
-      })
-    end
-
-    def hidden_content(hash)
-      "\n<!---\n@agileseason:#{hash}\n-->"
     end
   end
 end
