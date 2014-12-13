e770cfafc37a0ea6d21bc488f5314e7a788c2cb4
blackchestnut <alexander.kalinichev@gmail.com>
2014-12-13 17:27:20 +0300

    Cleanup code

--- app/models/board_history.rb
+++ app/models/board_history.rb
@@ -7,8 +7,7 @@ class BoardHistory < ActiveRecord::Base
   def update_data_issues(board_issues)
     issues_group = board_issues.each_with_object({}) { |pair, mem| mem[pair[0]] = pair[1].try(:size).to_i }
     total_issues = issues_group.sum { |e| e[1] }
-    data = []
-    board.columns.each_with_index() do |column, index|
+    self.data = board.columns.each_with_object([]) do |column, data|
       count = issues_group[column.label_name]
       data << {
         column_id: column.id,
@@ -17,6 +16,5 @@ class BoardHistory < ActiveRecord::Base
       }
       total_issues -= count
     end
-    self.data = data
   end
 end
