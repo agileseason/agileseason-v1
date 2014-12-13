3171ad3ae21f8595c1042a0146101b081b6e9916
blackchestnut <alexander.kalinichev@gmail.com>
2014-12-13 17:24:48 +0300

    Fix spec

--- spec/workers/graphs/cumulative_worker_spec.rb
+++ spec/workers/graphs/cumulative_worker_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Graphs::CumulativeWorker do
     before { perform }
 
     context :create_board_history do
-      let(:expected_data) { [{ column_id: 1, issues: 2 }] }
+      let(:expected_data) { [{ column_id: 1, issues: 2, issues_cumulative: 2 }] }
       it { is_expected.to have(1).item }
       it { expect(subject.first.data).to eq expected_data }
     end
