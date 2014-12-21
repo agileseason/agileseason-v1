1f839e82ed84c230aab86587920c79b539f688a0
blackchestnut <alexander.kalinichev@gmail.com>
2014-12-21 19:37:40 +0300

    #13 Add select with Labels on Issues#new

--- app/services/github_api/labels.rb
+++ app/services/github_api/labels.rb
@@ -1,11 +1,20 @@
 class GithubApi
   module Labels
     def sync_labels(board)
-      labels = client.labels(board.github_id).map(&:name)
+      labels = labels_all(board).map(&:name)
       missing_columns = board.columns.select { |column| column unless labels.include?(column.label_name) }
       missing_columns.each do |column|
         client.add_label(board.github_id, column.label_name, column.color)
       end
+    end
+
+    def labels(board)
+      columns_names = board.columns.map(&:label_name)
+      labels_all(board).select { |label| !columns_names.include?(label.name) }
+    end
+
+    def labels_all(board)
+      client.labels(board.github_id)
     end
   end
 end
---
+++ spec/services/github_api/labels_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe GithubApi::Labels do
+  let(:service) { GithubApi.new('fake_github_token') }
+  let(:board) { build(:board, columns: [column_1, column_2]) }
+  let(:column_1) { build(:column, name: 'backlog', order: 1) }
+  let(:column_2) { build(:column, name: 'todo', order: 2) }
+  let(:labels) { [label_1, label_2, label_3] }
+  let(:label_1) { OpenStruct.new(name: '[1] backlog') }
+  let(:label_2) { OpenStruct.new(name: '[2] todo') }
+  let(:label_3) { OpenStruct.new(name: 'feature') }
+  before { allow_any_instance_of(Octokit::Client).to receive(:labels).and_return(labels) }
+
+  describe '.labels' do
+    subject { service.labels(board) }
+
+    it { is_expected.to have(1).item }
+    it { expect(subject.first).to eq label_3 }
+  end
+
+  describe '.labels_all' do
+    subject { service.labels_all(board) }
+    it { is_expected.to have(3).items }
+  end
+end
