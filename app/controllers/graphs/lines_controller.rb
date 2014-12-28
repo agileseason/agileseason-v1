bb8e8c3d2ab50bbed38fddeb0b0cc97362a42978
blackchestnut <alexander.kalinichev@gmail.com>
2014-12-28 09:40:36 +0300

    #50 Fix dates on yAxis for Lines graphs

--- app/controllers/graphs/lines_controller.rb
+++ app/controllers/graphs/lines_controller.rb
@@ -1,7 +1,13 @@
 class Graphs::LinesController < ApplicationController
   before_action :fetch_board
+  helper_method :point_start
 
   def index
     @lines = @board.repo_histories.map(&:lines)
+  end
+
+  def point_start
+    history_date = @board.repo_histories.try(:first).try(:collected_on) || Date.today
+    Time.utc(history_date.year, history_date.month, history_date.day).to_i * 1000
   end
 end
