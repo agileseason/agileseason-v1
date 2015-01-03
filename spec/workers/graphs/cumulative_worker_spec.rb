7332b751356991f675bdc7033049045153fde87c
blackchestnut <alexander.kalinichev@gmail.com>
2015-01-03 23:05:52 +0300

    Try fix spec.

--- spec/workers/graphs/cumulative_worker_spec.rb
+++ spec/workers/graphs/cumulative_worker_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Graphs::CumulativeWorker do
   describe '.perform' do
     subject { board.board_histories }
     let(:board) { create(:board, :with_columns) }
+    let(:column) { board.columns.first }
     let(:perform) { worker.perform(board.id, 'fake_github_token') }
     let(:issue) { OpenStruct.new(name: 'issue_1') }
     before { allow_any_instance_of(GithubApi).to receive(:board_issues).and_return(board_issues) }
@@ -11,15 +12,15 @@ RSpec.describe Graphs::CumulativeWorker do
 
     context :create_board_history do
       context :one_issue do
-        let(:board_issues) { { board.columns.first.label_name => [issue] } }
-        let(:expected_data) { [{ column_id: 1, issues: 1, issues_cumulative: 1 }] }
+        let(:board_issues) { { column.label_name => [issue] } }
+        let(:expected_data) { [{ column_id: column.id, issues: 1, issues_cumulative: 1 }] }
         it { is_expected.to have(1).item }
         it { expect(subject.first.data).to eq expected_data }
       end
 
       context :two_issues do
         let(:board_issues) { { board.columns.first.label_name => [issue, issue] } }
-        let(:expected_data) { [{ column_id: 1, issues: 2, issues_cumulative: 2 }] }
+        let(:expected_data) { [{ column_id: column.id, issues: 2, issues_cumulative: 2 }] }
         it { is_expected.to have(1).item }
         it { expect(subject.first.data).to eq expected_data }
       end
