74806f57d0089f17ce80d9692c1ddb2d23a205ad
blackchestnut <alexander.kalinichev@gmail.com>
2015-01-13 21:20:57 +0300

    Change backup schedule.

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,6 +24,6 @@ env :PATH, ENV['PATH']
 
 job_type :backup, "cd :path/:backup_path && :environment_variable=:environment bundle exec backup perform -t :task --config_file ./config.rb :output"
 
-every 1.hours do
+every 4.hours do
   backup 'rails_database', backup_path: 'backup'
 end
