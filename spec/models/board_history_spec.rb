1f356bed5b7e3cf74a8197a1e57f8ed470d4c517
blackchestnut <alexander.kalinichev@gmail.com>
2015-01-17 10:47:43 +0300

    Fix duplicate date on Cumulative graph #72

--- spec/models/board_history_spec.rb
+++ spec/models/board_history_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe BoardHistory, type: :model do
   describe 'validates' do
     subject { BoardHistory.new }
     it { is_expected.to validate_presence_of(:collected_on) }
+    it { is_expected.to validate_uniqueness_of(:collected_on) }
   end
 
   describe '.update_data_issues' do
