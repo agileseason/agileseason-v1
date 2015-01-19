ab33ee7b45e39915a7b3938f1767a4327d44dbc6
Daria Sk <dashaskova@gmail.com>
2015-01-19 21:58:25 +0300

    try to cater to houndci

--- spec/services/github_api/comments_spec.rb
+++ spec/services/github_api/comments_spec.rb
@@ -4,13 +4,15 @@ describe GithubApi::Comments do
   let(:issue) { OpenStruct.new(number: 1) }
 
   let(:issue_comments) { [issue_comment_1, issue_comment_2] }
-  let(:issue_comment_1) { OpenStruct.new(
-    body: 'some example of code ```return blah;``` and comment') }
+  let(:issue_comment_1) do
+    OpenStruct.new(body: 'some example of code ```return blah;``` and comment')
+  end
   let(:issue_comment_2) { OpenStruct.new(body: 'some comment') }
 
   describe '#issue_comments' do
-    before { allow_any_instance_of(Octokit::Client)
-      .to receive(:issue_comments).and_return(issue_comments) }
+    before do
+      allow_any_instance_of(Octokit::Client).to receive(:issue_comments).and_return(issue_comments)
+    end
     subject { service.issue_comments(board.github_id, issue.number) }
     it { is_expected.to have(2).items }
   end
