4aa8939554cc720d25b9351b65976dc55fe8d008
blackchestnut <alexander.kalinichev@gmail.com>
2015-01-23 07:24:51 +0300

    BoardPresenter#name

--- app/presenters/board_presenter.rb
+++ app/presenters/board_presenter.rb
@@ -1,7 +1,13 @@
 class BoardPresenter < Keynote::Presenter
   presents :board
 
-  def board_name
+  def name
+    raw("#{board.name.try(:gsub, ' ', '&nbsp;')}&nbsp;#{board_type}")
+  end
+
+  private
+
+  def board_type
     board.type
       .split('::').last
       .split('Board').first.downcase
