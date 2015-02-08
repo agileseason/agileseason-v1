071de7673825bb1987ccf6140b78f4fb1fe546f1
Daria <darya.office@gmail.com>
2015-02-08 22:06:10 +0300

    fix after MR

--- app/services/github_api/comments.rb
+++ app/services/github_api/comments.rb
@@ -11,16 +11,5 @@ class GithubApi
     def update_comment(board, number, comment)
       client.update_comment(board.github_id, number, comment)
     end
-
-    #def add_comment(options)
-      #client.create_pull_request_comment(
-        #options[:commit].repo_name,
-        #options[:pull_request_number],
-        #options[:comment],
-        #options[:commit].sha,
-        #options[:filename],
-        #options[:patch_position]
-      #)
-    #end
   end
 end
