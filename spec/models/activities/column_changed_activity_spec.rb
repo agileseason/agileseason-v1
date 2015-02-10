9b9faf642572b83088ef19d1e642b4f3880ad67f
blackchestnut <alexander.kalinichev@gmail.com>
2015-02-10 08:55:29 +0300

    Fix filename and spec.

--- spec/models/activities/column_changed_activity_spec.rb
+++ spec/models/activities/column_changed_activity_spec.rb
@@ -4,12 +4,12 @@ describe Activities::ColumnChangedActivity, type: :model do
     let(:user) { build_stubbed(:user) }
     let(:board) { create(:board, :with_columns, user: user) }
     let(:issue_stat) { create(:issue_stat, board: board) }
-    let(:column_from) { 'todo' }
-    let(:column_to) { 'progress' }
+    let(:column_from) { build(:column, name: 'todo') }
+    let(:column_to) { build(:column, name: 'progress') }
 
     it { expect { subject }.to change(Activities::ColumnChangedActivity, :count).by(1) }
     it { expect(subject.board).to eq board }
     it { expect(subject.issue_stat).to eq issue_stat }
-    it { expect(subject.data).to eq({ column_from: column_from, column_to: column_to }) }
+    it { expect(subject.data).to eq({ column_from: column_from.name, column_to: column_to.name }) }
   end
 end
