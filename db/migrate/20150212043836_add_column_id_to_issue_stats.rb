389e3bb4eb591c7ce41ab796b3268a3920550b77
blackchestnut <alexander.kalinichev@gmail.com>
2015-02-12 08:49:10 +0300

    init issue_stat.column in migration

--- db/migrate/20150212043836_add_column_id_to_issue_stats.rb
+++ db/migrate/20150212043836_add_column_id_to_issue_stats.rb
@@ -1,6 +1,15 @@
 class AddColumnIdToIssueStats < ActiveRecord::Migration
-  def change
+  def up
     add_column :issue_stats, :column_id, :integer
     add_index :issue_stats, [:column_id]
+
+    IssueStat.all.each do |issue_stat|
+      issue_stat.update(column: issue_stat.board.columns.first) if issue_stat.board && !issue_stat.column
+    end
+  end
+
+  def down
+    remove_index :issue_stats, [:column_id]
+    remove_column :issue_stats, :column_id
   end
 end
