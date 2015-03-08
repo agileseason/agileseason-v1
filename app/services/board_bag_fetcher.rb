3519dca1732ad7cb03b8c47f51f7196838b33d2b
blackchestnut <alexander.kalinichev@gmail.com>
2015-03-08 14:44:53 +0300

    Added async fetcher #225

---
+++ app/services/board_bag_fetcher.rb
@@ -0,0 +1,11 @@
+class BoardBagFetcher < BoardBag
+  pattr_initialize :github_api, :board
+
+  def refresh_collaborators
+    Rails.cache.write(cache_key(:collaborators), @github_api.collaborators(@board), expires_in: 20.minutes)
+  end
+
+  def refresh_labels
+    Rails.cache.write(cache_key(:labels), @github_api.labels(@board), expires_in: 20.minutes)
+  end
+end
