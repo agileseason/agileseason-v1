715fd25088225d517a91c357282afb1a796ff314
blackchestnut <alexander.kalinichev@gmail.com>
2015-03-29 16:45:05 +0300

    Added joined boards to menu #139

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,7 +1,16 @@
 class UserPresenter < Keynote::Presenter
   presents :user
+  delegate :github_api, to: :user
 
   def boards
-    user.boards.select(&:persisted?).sort_by(&:name)
+    (owned_boards + joined_boards).sort_by(&:name)
+  end
+
+  def owned_boards
+    @owned_boards ||= user.boards.select(&:persisted?)
+  end
+
+  def joined_boards
+    @joined_boards ||= Board.where(github_name: github_api.cached_repos.map(&:name))
   end
 end
