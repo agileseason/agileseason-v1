c1ff514cef24359a745bbb0bed418cca4bffa2a7
blackchestnut <alexander.kalinichev@gmail.com>
2015-03-30 22:37:38 +0300

    Added spec to board_bag#column_issues before refactoring.

--- spec/view_objects/board_bag_spec.rb
+++ spec/view_objects/board_bag_spec.rb
@@ -33,4 +33,55 @@ describe BoardBag do
     it { is_expected.to be_a(Issue) }
     its(:labels) { is_expected.to eq ['label_1'] }
   end
+
+  describe '#column_issues' do
+    let(:board) { build(:board, columns: [column_1, column_2]) }
+    let(:column_1) { build_stubbed(:column, name: 'backlog', order: 1, issues: issues) }
+    let(:column_2) { build_stubbed(:column, name: 'todo', order: 2) }
+    let(:issue_1_1) do
+      build_stubbed(
+        :issue_stat,
+        column: column_1,
+        number: github_issue_1.number
+      )
+    end
+    let(:issue_2_1) do
+      build_stubbed(
+        :issue_stat,
+        column: column_1,
+        number: github_issue_2.number
+      )
+    end
+    let(:issue_3_1) do
+      build_stubbed(
+        :issue_stat,
+        column: column_1,
+        number: github_issue_3.number,
+        archived_at: Time.now
+      )
+    end
+    let(:github_issue_1) { OpenStruct.new(number: 1) }
+    let(:github_issue_2) { OpenStruct.new(number: 2) }
+    let(:github_issue_3) { OpenStruct.new(number: 3, archive?: true) }
+    subject { bag.column_issues(column_1) }
+    before do
+      allow_any_instance_of(GithubApi).
+        to receive(:board_issues).
+          and_return(
+            { column_1.id => [github_issue_1, github_issue_2, github_issue_3] }
+          )
+    end
+
+    context 'with column.issues', focus: true do
+      let(:issues) { [github_issue_2.number, github_issue_1.number] }
+      it { is_expected.to have(2).items }
+      it { expect(subject.first).to eq github_issue_2 }
+    end
+
+    context 'without column.issues' do
+      let(:issues) { nil }
+      it { is_expected.to have(2).items }
+      it { expect(subject.first).to eq github_issue_1 }
+    end
+  end
 end
