1e59f44093bad855b80054df4c39958fde32d604
blackchestnut <alexander.kalinichev@gmail.com>
2015-03-31 21:50:48 +0300

    Reformat long lines.

--- app/services/board_bag_fetcher.rb
+++ app/services/board_bag_fetcher.rb
@@ -2,14 +2,26 @@ class BoardBagFetcher < BoardBag
   pattr_initialize :github_api, :board
 
   def refresh_collaborators
-    Rails.cache.write(cache_key(:collaborators), @github_api.collaborators(@board), expires_in: 20.minutes)
+    Rails.cache.write(
+      cache_key(:collaborators),
+      @github_api.collaborators(@board),
+      expires_in: 20.minutes
+    )
   end
 
   def refresh_labels
-    Rails.cache.write(cache_key(:labels), @github_api.labels(@board), expires_in: 20.minutes)
+    Rails.cache.write(
+      cache_key(:labels),
+      @github_api.labels(@board),
+      expires_in: 20.minutes
+    )
   end
 
   def refresh_issues
-    Rails.cache.write(cache_key(:board_issues), @github_api.board_issues(@board), expires_in: 5.minutes)
+    Rails.cache.write(
+      cache_key(:board_issues),
+      @github_api.board_issues(@board),
+      expires_in: 5.minutes
+    )
   end
 end
