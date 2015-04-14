83aba62eb90e6828611f894a9c1d936a7faf4a5c
blackchestnut <alexander.kalinichev@gmail.com>
2015-04-14 17:38:36 +0300

    Simple intervals for cumulative graphs #260

--- app/controllers/graphs/cumulative_controller.rb
+++ app/controllers/graphs/cumulative_controller.rb
@@ -2,6 +2,12 @@ class Graphs::CumulativeController < ApplicationController
   before_action :fetch_board
 
   def index
-    @series = Graphs::CumulativeBuilder.new(@board).series
+    @series = Graphs::CumulativeBuilder.new(@board, interval).series
+  end
+
+  private
+
+  def interval
+    params[:interval] && params[:interval] == 'month' ? :month : :all
   end
 end
