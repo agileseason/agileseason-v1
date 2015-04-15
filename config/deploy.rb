2f629126f1c17a831b05976ff9bce27c9b5f6756
blackchestnut <alexander.kalinichev@gmail.com>
2015-04-15 17:12:39 +0300

    Revert rbenv_ruby version.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'agileseason'
 set :repo_url, 'git@github.com:agileseason/agileseason.git'
 
 set :rbenv_type, :system
-set :rbenv_ruby, '2.1.5'
+set :rbenv_ruby, '2.1.3'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
