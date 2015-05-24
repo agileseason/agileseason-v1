0544d2972e38e705fd8e4ad8573cef0e9edf1468
blackchestnut <alexander.kalinichev@gmail.com>
2015-05-24 11:15:25 +0300

    Fix sum total issues in BoardHistory#update_data_issues

--- app/models/board_history.rb
+++ app/models/board_history.rb
@@ -9,7 +9,7 @@ class BoardHistory < ActiveRecord::Base
   # FIX : do something with this method!
   def update_data_issues(board_issues)
     issues_group = board_issues.each_with_object({}) { |pair, mem| mem[pair[0]] = pair[1].try(:size).to_i }
-    total_issues = issues_group.sum { |e| e[1] }
+    total_issues = issues_group.sum { |e| e[1] } || 0
     self.data = {}
     self.data = board.columns.each_with_object([]) do |column, data|
       count = issues_group[column.id]
