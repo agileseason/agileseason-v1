e13128a610afad3586f4b66777c49289a7970dab
blackchestnut <alexander.kalinichev@gmail.com>
2015-05-24 11:28:50 +0300

    Fix spec.

--- spec/workers/graphs/cumulative_worker_spec.rb
+++ spec/workers/graphs/cumulative_worker_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Graphs::CumulativeWorker do
   describe '.perform' do
     subject { board.board_histories }
     let(:board) { create(:board, :with_columns) }
-    let(:column) { board.columns.first }
+    let(:column_1) { board.columns.first }
+    let(:column_2) { board.columns.second }
     let(:perform) { worker.perform(board.id, 'fake_token') }
     let(:issue) { BoardIssue.new(nil, nil) }
     before do
@@ -15,15 +16,25 @@ RSpec.describe Graphs::CumulativeWorker do
 
     context :create_board_history do
       context :one_issue do
-        let(:board_issues) { { column.id => [issue] } }
-        let(:expected_data) { [{ column_id: column.id, issues: 1, issues_cumulative: 1 }] }
+        let(:board_issues) { { column_1.id => [issue] } }
+        let(:expected_data) do
+          [
+            { column_id: column_1.id, issues: 1, issues_cumulative: 1 },
+            { column_id: column_2.id, issues: 0, issues_cumulative: 0 },
+          ]
+        end
         it { is_expected.to have(1).item }
         it { expect(subject.first.data).to eq expected_data }
       end
 
       context :two_issues do
-        let(:board_issues) { { column.id => [issue, issue] } }
-        let(:expected_data) { [{ column_id: column.id, issues: 2, issues_cumulative: 2 }] }
+        let(:board_issues) { { column_1.id => [issue, issue] } }
+        let(:expected_data) do
+          [
+            { column_id: column_1.id, issues: 2, issues_cumulative: 2 },
+            { column_id: column_2.id, issues: 0, issues_cumulative: 0 },
+          ]
+        end
         it { is_expected.to have(1).item }
         it { expect(subject.first.data).to eq expected_data }
       end
