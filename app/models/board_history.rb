a7d1a16de365fb53897b41e2ec612554892edb98
blackchestnut <alexander.kalinichev@gmail.com>
2015-05-24 11:20:48 +0300

    Fix count issues in BoardHistory#update_data_issues

--- app/models/board_history.rb
+++ app/models/board_history.rb
@@ -9,10 +9,10 @@ class BoardHistory < ActiveRecord::Base
   # FIX : do something with this method!
   def update_data_issues(board_issues)
     issues_group = board_issues.each_with_object({}) { |pair, mem| mem[pair[0]] = pair[1].try(:size).to_i }
-    total_issues = issues_group.sum { |e| e[1] } || 0
+    total_issues = issues_group.sum { |e| e[1] }
     self.data = {}
     self.data = board.columns.each_with_object([]) do |column, data|
-      count = issues_group[column.id]
+      count = issues_group[column.id] || 0
       data << {
         column_id: column.id,
         issues: count,
