577c5d66b4cac637ba2b85aff667592cda2ecba2
blackchestnut <alexander.kalinichev@gmail.com>
2015-05-25 07:14:39 +0300

    Redirect to boards_url from landing if signed in.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,5 +2,6 @@ class LandingController < ApplicationController
   skip_before_filter :authenticate, unless: -> { current_user }
 
   def index
+    return redirect_to boards_url if current_user
   end
 end
