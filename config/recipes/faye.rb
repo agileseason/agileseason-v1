7f1aadd478fe2c6de77d00b890a4cb17636295a1
blackchestnut <alexander.kalinichev@gmail.com>
2015-06-22 20:46:11 +0300

    Code cleanup

--- config/recipes/faye.rb
+++ config/recipes/faye.rb
@@ -2,14 +2,14 @@ set_default(:faye_user) { user }
 set_default(:faye_pid) { "#{current_path}/tmp/pids/faye.pid" }
 
 namespace :faye do
-  desc "Setup Faye initializer"
+  desc 'Setup Faye initializer'
   task :setup, roles: :app do
-    template "faye_init", "/tmp/faye_init"
-    run "chmod +x /tmp/faye_init"
+    template 'faye_init', '/tmp/faye_init'
+    run 'chmod +x /tmp/faye_init'
     run "#{sudo} mv /tmp/faye_init /etc/init.d/faye_#{application}"
     run "#{sudo} update-rc.d -f faye_#{application} defaults"
   end
-  after "deploy:setup", "faye:setup"
+  after 'deploy:setup', 'faye:setup'
 
   %w[start stop restart].each do |command|
     desc "#{command} faye"
