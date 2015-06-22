3268435383619783f45b979f1b0f2f58ccdab466
blackchestnut <alexander.kalinichev@gmail.com>
2015-06-22 20:55:08 +0300

    Add linked dir for faye configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml initializers/faye_token.rb}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
