76122be62bde552f36a51508f0e5ab00744dbfa9
blackchestnut <alexander.kalinichev@gmail.com>
2015-06-28 21:33:32 +0300

    Add path to fonts #382

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,5 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
-config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 Rails.application.config.assets.precompile += %w( .otf .ttf )
