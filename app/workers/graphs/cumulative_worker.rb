0d5c0f513e7b4a293a65011aaeeea799e08c5d8c
blackchestnut <alexander.kalinichev@gmail.com>
2015-06-29 22:38:35 +0300

    Hot fix for cumulative graph #446

--- app/workers/graphs/cumulative_worker.rb
+++ app/workers/graphs/cumulative_worker.rb
@@ -15,8 +15,7 @@ module Graphs
 
     def save_current_history(board, issues_by_columns)
       board_history = fetch_board_history(board)
-      board_history.update_data_issues(issues_by_columns)
-      board_history.save
+      board_history.update(data: fetch_data(board, issues_by_columns))
       board_history
     end
 
@@ -30,6 +29,36 @@ module Graphs
 
     def build_board_history(board)
       board.board_histories.build(collected_on: Date.today)
+    end
+
+    def fetch_data(board, board_issues)
+      issues_group = board_issues.each_with_object({}) do |pair, hash|
+        column_id = pair[0]
+        issues = pair[1]
+        hash[column_id] = calc_issues(issues)
+      end
+
+      total_issues = total_issues_count(board, issues_group)
+      data = {}
+      data = board.columns.each_with_object([]) do |column, arr|
+        count = issues_group[column.id] || 0
+        arr << {
+          column_id: column.id,
+          issues: count,
+          issues_cumulative: total_issues
+        }
+        total_issues -= count
+      end
+    end
+
+    def calc_issues(issues)
+      return 0 if issues.blank?
+      issues.select { |issue| !issue.archive? }.size
+    end
+
+    def total_issues_count(board, issues_group)
+      board_total = issues_group.sum { |e| e[1] }
+      board.issue_stats.archived.count + board_total
     end
   end
 end
