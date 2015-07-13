8817caaa7e68c46dbfaf9e8a6cf7aa76597a2728
blackchestnut <alexander.kalinichev@gmail.com>
2015-07-13 21:32:12 +0300

    Remove target blank from issue link #506

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -22,7 +22,7 @@ describe ApplicationHelper do
       context 'with #number' do
         let(:text) { "##{number}" }
         let(:number) { 10 }
-        it { is_expected.to eq "<p><a href='#{repo_url}/issues/#{number}' target='_blank'>##{number}</a></p>\n" }
+        it { is_expected.to eq "<p><a href='#{repo_url}/issues/#{number}'>##{number}</a></p>\n" }
       end
 
       context 'with #abs' do
@@ -39,7 +39,7 @@ describe ApplicationHelper do
         let(:text) { "text ##{number_1} text2 ##{number_2}" }
         let(:number_1) { 10 }
         let(:number_2) { 999 }
-        it { is_expected.to eq "<p>text <a href='#{repo_url}/issues/#{number_1}' target='_blank'>##{number_1}</a> text2 <a href='http://github.com/a/b/issues/#{number_2}' target='_blank'>##{number_2}</a></p>\n" }
+        it { is_expected.to eq "<p>text <a href='#{repo_url}/issues/#{number_1}'>##{number_1}</a> text2 <a href='http://github.com/a/b/issues/#{number_2}'>##{number_2}</a></p>\n" }
       end
     end
 
