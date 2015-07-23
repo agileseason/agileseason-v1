3ef7a92d27830d7a5ffe3df0fc69bb81d4f94976
blackchestnut <alexander.kalinichev@gmail.com>
2015-07-23 08:19:34 +0300

    Format code

--- app/presenters/issue_presenter.rb
+++ app/presenters/issue_presenter.rb
@@ -24,23 +24,24 @@ class IssuePresenter < Keynote::Presenter
 
   def color_for(label)
     case label.color
-      # Github standart colors
-      when 'ffffff' then '000'
-      when 'e6e6e6' then '000'
-      when 'fef2c0' then '000'
-      when '84b6eb' then '000'
-      when 'cccccc' then '000'
-      when 'fbca04' then '000'
-      when 'f7c6c7' then '000'
-      when 'fad8c7' then '000'
-      when 'fef2c0' then '000'
-      when 'bfe5bf' then '000'
-      when 'bfdadc' then '000'
-      when 'c7def8' then '000'
-      when 'bfd4f2' then '000'
-      when 'd4c5f9' then '000'
+    # Github standart colors
+    when 'ffffff' then '000'
+    when 'e6e6e6' then '000'
+    when 'fef2c0' then '000'
+    when '84b6eb' then '000'
+    when 'cccccc' then '000'
+    when 'fbca04' then '000'
+    when 'f7c6c7' then '000'
+    when 'fad8c7' then '000'
+    when 'fef2c0' then '000'
+    when 'bfe5bf' then '000'
+    when 'bfdadc' then '000'
+    when 'c7def8' then '000'
+    when 'bfd4f2' then '000'
+    when 'd4c5f9' then '000'
 
-      else 'fff'
+    else
+      'fff'
     end
   end
 
