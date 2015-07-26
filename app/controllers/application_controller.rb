77b7425f59ee4833dd37570f56216310dd775cf8
blackchestnut <alexander.kalinichev@gmail.com>
2015-07-26 06:51:14 +0300

    Return force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #before_action :force_https
+  before_action :force_https
   before_action :authenticate
   before_action :track_guest, unless: :signed_in?
 
