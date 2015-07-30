f087d1328f58f089afee9dcef9ad884e04de4670
blackchestnut <alexander.kalinichev@gmail.com>
2015-07-30 23:27:53 +0300

    Remove ui_event for signed_in users

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,7 +2,7 @@ class LandingController < ApplicationController
   skip_before_filter :authenticate, unless: -> { current_user }
 
   def index
-    ui_event(:landing)
     return redirect_to boards_url if current_user
+    ui_event(:landing)
   end
 end
--- spec/controllers/landing_controller_spec.rb
+++ spec/controllers/landing_controller_spec.rb
@@ -1,11 +1,23 @@
 RSpec.describe LandingController, type: :controller do
   render_views
 
-  describe 'GET index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
+  describe '#index' do
+    let(:request) { get :index }
+    before { allow(controller).to receive(:ui_event) }
+
+    context 'signed_in' do
+      before { stub_sign_in }
+      before { request }
+
+      it { expect(response).to redirect_to(boards_url) }
+      it { expect(controller).not_to have_received(:ui_event) }
+    end
+
+    context 'not signed_in' do
+      before { request }
+
+      it { expect(response).to have_http_status(:success) }
+      it { expect(controller).to have_received(:ui_event) }
     end
   end
-
 end
