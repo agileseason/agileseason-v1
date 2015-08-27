ef84c092d2e0e606dea2e99be52f7c45680e2d35
blackchestnut <alexander.kalinichev@gmail.com>
2015-08-27 08:40:57 +0300

    Add spec for roadmaps controller

--- spec/controllers/roadmaps_controller_spec.rb
+++ spec/controllers/roadmaps_controller_spec.rb
@@ -1,7 +1,11 @@
 describe RoadmapsController do
   describe '#show' do
+    let(:board) { create(:board, :with_columns, user: user) }
+    let(:user) { create(:user) }
+    before { stub_sign_in(user) }
+
     it 'returns http success' do
-      get :show
+      get :show, board_github_full_name: board.github_full_name
       expect(response).to have_http_status(:success)
     end
   end
