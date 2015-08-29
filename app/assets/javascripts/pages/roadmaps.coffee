6686e189205171bd153ecfa6160d176dbe67e077
blackchestnut <alexander.kalinichev@gmail.com>
2015-08-29 22:17:14 +0300

    Add scaling and forecast closed_at for open issues

--- app/assets/javascripts/pages/roadmaps.coffee
+++ app/assets/javascripts/pages/roadmaps.coffee
@@ -5,19 +5,29 @@ $(document).on 'page:change', ->
 
   # Issues
   issues = $canvas.data('chart-issues')
-  issueHeigth = 40
-  rowOffset = 20
+  issueHeigth = 30
+  rowOffset = 2
+  max_issue_row = d3.max(issues, (e) -> e.row + 1) # Plus 1 for xAxis
 
   # Dates as xAxis
   yXAxis = (issueHeigth + rowOffset) * $canvas.data('chart-issue-rows')
   dates = $canvas.data('chart-dates')
 
   # Prepare svg canvas
+  chart_height = max_issue_row * (issueHeigth + rowOffset)
+  chart_width = 1300
   svg = d3.select('.canvas')
     .append('svg')
     .attr
-      height: 280
-      width: 2000
+      height: chart_height
+      width: chart_width
+
+  # Scale
+  max_issue_from = d3.max(issues, (e) -> e.from + e.cycletime)
+  xScale = d3.scale.
+    linear().
+    domain([0, max_issue_from]). # Data minimum and maximum
+    range([0, chart_width])  # pixels to map
 
   # Issue Cycletime Rectangle
   svg.selectAll('rect')
@@ -25,10 +35,10 @@ $(document).on 'page:change', ->
     .enter()
     .append('rect')
       .attr
-        class: 'issue'
-        width: (e) -> e.cycletime
+        class: (e) -> "issue #{e.state}"
+        width: (e) -> xScale(e.cycletime)
         height: issueHeigth
-        x: (e) -> e.from
+        x: (e) -> xScale(e.from)
         y: (e, i) ->
           e.row * (issueHeigth + rowOffset)
 
@@ -39,11 +49,11 @@ $(document).on 'page:change', ->
       .append('text')
         .attr
           class: 'issue-number'
-          x: (e) -> e.from # Copy paste from rect - issues
+          x: (e) -> xScale(e.from) # Copy paste from rect - issues
           y: (e) ->
             e.row * (issueHeigth + rowOffset)
           dx: 6
-          dy: issueHeigth / 2
+          dy: issueHeigth / 1.6
         .text((e) -> "##{e.number}")
 
   # xAxis as Dates
@@ -53,6 +63,6 @@ $(document).on 'page:change', ->
       .append('text')
       .attr
         class: 'x-axis'
-        x: (e) -> e.from
+        x: (e) -> xScale(e.from)
         y: (e) -> yXAxis
       .text((e) -> e.text)
--- app/controllers/roadmaps_controller.rb
+++ app/controllers/roadmaps_controller.rb
@@ -3,6 +3,9 @@ class RoadmapsController < ApplicationController
 
   helper_method :chart_issues, :chart_dates, :chart_issue_rows
 
+  ISSUE_WIDTH = 40
+  NORM_COEFF = 1.0 / 86400 * ISSUE_WIDTH
+
   def show
   end
 
@@ -13,22 +16,79 @@ class RoadmapsController < ApplicationController
   end
 
   def chart_dates
-    [
-      { from: 0, text: '06 Aug' },
-      { from: 60, text: '07 Aug' }
-    ].to_json
+    @chart_dates ||= issue_stats.
+      uniq { |i| i.created_at.to_date }.
+      map do |i|
+        {
+          from: (i.created_at.to_i - normalization_from) * NORM_COEFF,
+          text: i.created_at.strftime('%d %b')
+        }
+      end.
+      uniq { |date| date[:from] }. # TODO Fix normalization error if dates are close.
+      to_json
   end
 
   def chart_issue_rows
-    issues.max { |e| e[:row] }[:row] + 1
+    issues.map { |e| e[:row] }.max + 1
   end
 
   def issues
-    @issues ||=
-      [
-        { row: 0, from: 0, cycletime: 40, number: 84 },
-        { row: 1, from: 0, cycletime: 140, number: 90 },
-        { row: 0, from: 60, cycletime: 90, number: 75 }
-      ]
+    @min_forecast_closed_at = Time.current
+    issues = issue_stats.
+      map do |i|
+        {
+          number: i.number,
+          from: (i.created_at.to_i - normalization_from) * NORM_COEFF,
+          cycletime: cycletime(i),
+          state: i.closed_at.nil? ? 'open' : 'closed'
+        }
+      end
+    rows_optimizer(issues)
+  end
+
+  def rows_optimizer(issues)
+    rows = []
+    max_index = 0
+    issues.each do |issue|
+      index = rows.index { |r| r < issue[:from] }
+      if index.nil?
+        index = max_index
+        max_index += 1
+      end
+      rows[index] = issue[:from] + issue[:cycletime]
+      issue[:row] = index
+    end
+
+    issues
+  end
+
+  def cycletime(issue)
+    closed_at = if issue.closed_at.present?
+      issue.closed_at
+    else
+      prev = @min_forecast_closed_at
+      @min_forecast_closed_at += avg_cycletime
+      prev
+    end
+    (closed_at.to_i - issue.created_at.to_i) * NORM_COEFF
+  end
+
+  def avg_cycletime
+    3.days
+  end
+
+  def normalization_from
+    @normalization_from ||= issue_stats.map(&:created_at).min.try(:to_i) || 1
+  end
+
+  def issue_stats
+    #@issue_stats ||= @board.issue_stats.order(:created_at)
+    @issue_stats ||= [
+      IssueStat.new(number: 1, created_at: Time.current - 10.days, closed_at: Time.current - 1.day),
+      IssueStat.new(number: 2, created_at: Time.current - 5.days, closed_at: Time.current - 3.days - 4.hours),
+      IssueStat.new(number: 3, created_at: Time.current - 3.days, closed_at: Time.current - 1.day),
+      IssueStat.new(number: 4, created_at: Time.current - 3.days, closed_at: nil),
+      IssueStat.new(number: 5, created_at: Time.current - 2.days, closed_at: nil),
+    ]
   end
 end
