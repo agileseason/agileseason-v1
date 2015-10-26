8b1f804994d045d8168b59e2fed667761df1822f
blackchestnut <alexander.kalinichev@gmail.com>
2015-10-26 08:04:00 +0300

    Use Time.zone in spec

--- spec/services/graphs/roadmap_issue_spec.rb
+++ spec/services/graphs/roadmap_issue_spec.rb
@@ -9,7 +9,7 @@ describe RoadmapIssue do
       )
     end
     let(:issue) { BoardIssue.new(github_issue, issue_stat) }
-    let(:free_time_at) { Time.parse('2015-10-01') }
+    let(:free_time_at) { Time.zone.parse('2015-10-01') }
     let(:cycle_time_days) { 2 }
     let(:created_at) { free_time_at - 1.day }
 
@@ -58,7 +58,7 @@ describe RoadmapIssue do
         let!(:lifetime) { create(:lifetime, issue_stat: issue_stat, column: column, in_at: in_at, out_at: out_at) }
 
         context 'in and out' do
-          let(:in_at) { Time.parse('2015-10-01') }
+          let(:in_at) { Time.zone.parse('2015-10-01') }
           let(:out_at) { in_at + 1.day }
 
           it { is_expected.not_to be_nil }
@@ -89,10 +89,11 @@ describe RoadmapIssue do
       end
 
       context 'with lifetimes' do
+        let(:in_at_1) { Time.zone.parse('2015-10-01') }
+
         context 'in but not out - last column' do
           let!(:lifetime_1) { create(:lifetime, issue_stat: issue_stat, column: column, in_at: in_at_1, out_at: out_at_1) }
           let!(:lifetime_2) { create(:lifetime, issue_stat: issue_stat, column: column, in_at: out_at_1, out_at: nil) }
-          let(:in_at_1) { Time.parse('2015-10-01') }
           let(:out_at_1) { in_at_1 + 1.day }
 
           it { is_expected.not_to be_nil }
@@ -105,7 +106,6 @@ describe RoadmapIssue do
           let!(:lifetime_2) { create(:lifetime, issue_stat: issue_stat, column: column_2, in_at: out_at_1, out_at: nil) }
           let(:column_1) { board.columns.first }
           let(:column_2) { board.columns.last }
-          let(:in_at_1) { Time.parse('2015-10-01') }
           let(:out_at_1) { in_at_1 + 1.day }
           let(:column_ids) { [column_1] }
 
