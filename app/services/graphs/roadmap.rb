bcf404391f25dbae5b8bf54a18d055f8eaf07f15
blackchestnut <alexander.kalinichev@gmail.com>
2015-10-26 08:19:46 +0300

    Extract method for build roadmap_chart_issues and code cleanup

--- app/services/graphs/roadmap.rb
+++ app/services/graphs/roadmap.rb
@@ -8,7 +8,6 @@ class Roadmap
   ISSUE_WIDTH = 40 # Pixels
   NORM_COEFF = 1.0 / 86400 * ISSUE_WIDTH
 
-  # TODO Filtering dates by lifetimes
   def call
     OpenStruct.new(issues: issues, dates: dates, current_date: current_date)
   end
@@ -17,32 +16,36 @@ class Roadmap
 
   def issues
     @min_forecast_closed_at = Time.current
-    issues = issue_stats.map do |i|
-      github_issue = board_bag.issues_hash[i.number]
+    issues = issue_stats.map do |issue_stat|
       roadmap_issue = RoadmapIssue.call(
-        issue_stat: i,
+        issue_stat: issue_stat,
         free_time_at: @min_forecast_closed_at,
         cycle_time_days: avg_days_per_issue,
         column_ids: column_ids
       )
-
       next if roadmap_issue.nil?
 
       @min_forecast_closed_at = roadmap_issue.free_time_at
-      {
-        number: i.number,
-        title: github_issue.present? ? github_issue.title : '<unknown>',
-        from: (roadmap_issue.from.to_i - min_from) * NORM_COEFF,
-        cycletime: roadmap_issue.cycletime.to_i * NORM_COEFF,
-        state: i.state,
-        created_at: i.created_at.strftime('%d %b'),
-        closed_at: "#{roadmap_issue.to.strftime('%d %b')} #{'(?)' unless i.closed?}",
-        column: i.column.name,
-        is_archive: i.archived?
-      }
+      github_issue = board_bag.issues_hash[issue_stat.number]
+      build_chart_issue(issue_stat, github_issue, roadmap_issue)
     end.compact
 
     rows_optimizer(issues)
+  end
+
+  # TODO Extract RoadmapIssue (and rename service)
+  def build_chart_issue(i, github_issue, roadmap_issue)
+    {
+      number: i.number,
+      title: github_issue.present? ? github_issue.title : '<unknown>',
+      from: (roadmap_issue.from.to_i - min_from) * NORM_COEFF,
+      cycletime: roadmap_issue.cycletime.to_i * NORM_COEFF,
+      state: i.state,
+      created_at: i.created_at.strftime('%d %b'),
+      closed_at: "#{roadmap_issue.to.strftime('%d %b')} #{'(?)' unless i.closed?}",
+      column: i.column.name,
+      is_archive: i.archived?
+    }
   end
 
   def dates
