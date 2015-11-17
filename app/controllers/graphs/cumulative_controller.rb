2dbb5e726ae86d9295d2f154c02988e5581494e5
blackchestnut <alexander.kalinichev@gmail.com>
2015-11-18 00:09:46 +0300

    Hotfix - don`t run cumulative_worker if user has not got permissions #634

--- app/controllers/graphs/cumulative_controller.rb
+++ app/controllers/graphs/cumulative_controller.rb
@@ -13,6 +13,7 @@ class Graphs::CumulativeController < ApplicationController
   end
 
   def fetch_cumulative_graph
+    return unless @board_bag.has_write_permission?
     return unless @board.board_histories.where(collected_on: Date.today).blank?
     Graphs::CumulativeWorker.perform_async(@board.id, encrypted_github_token)
   end
--- spec/controllers/graphs/cumulative_controller_spec.rb
+++ spec/controllers/graphs/cumulative_controller_spec.rb
@@ -4,7 +4,13 @@ describe Graphs::CumulativeController, type: :controller do
     let(:user) { create(:user) }
     let(:board) { create(:board, :with_columns, user: user) }
     let(:interval) {}
+    let(:is_owner) { true }
     before { allow(Graphs::CumulativeWorker).to receive(:perform_async) }
+    before do
+      allow_any_instance_of(BoardBag).
+        to receive(:has_write_permission?).
+        and_return(is_owner)
+    end
     before { stub_sign_in(user) }
 
     context 'responce' do
@@ -30,13 +36,22 @@ describe Graphs::CumulativeController, type: :controller do
       before { board_history }
       before { subject }
 
+      context 'without permissions' do
+        let(:is_owner) { false }
+        let(:board_history) {}
+
+        it { expect(Graphs::CumulativeWorker).not_to have_received(:perform_async) }
+      end
+
       context 'without board history' do
         let(:board_history) { create(:board_history, board: board, collected_on: 1.day.ago) }
+
         it { expect(Graphs::CumulativeWorker).to have_received(:perform_async) }
       end
 
       context 'with board history' do
         let(:board_history) { create(:board_history, board: board, collected_on: Date.today) }
+
         it { expect(Graphs::CumulativeWorker).not_to have_received(:perform_async) }
       end
     end
