33edfd3a6e008bb9597df0ee3cbd6673189cb2b0
blackchestnut <alexander.kalinichev@gmail.com>
2015-11-17 23:43:31 +0300

    Try fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:agileseason/agileseason.git'
 set :rbenv_type, :system
 set :rbenv_ruby, '2.2.3'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
