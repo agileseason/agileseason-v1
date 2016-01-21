4c522eb6e31df3182a9db70fc6a7fb0d20292605
blackchestnut <alexander.kalinichev@gmail.com>
2016-01-21 09:06:58 +0300

    Tune board export (html, markdown, text) #764

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -1,9 +1,23 @@
 class ExportsController < ApplicationController
-  layout false
-
   before_action :fetch_board
+  helper_method :current_type
 
   def show
-    render 'exports/board.text', content_type: 'text/plain'
+    #render 'exports/board.text', content_type: 'text/plain'
+  end
+
+  private
+
+  def current_type
+    case params[:type]
+      when 'html'
+        :html
+      when 'text'
+        :text
+      when 'markdown'
+        :markdown
+      else
+        :markdown
+    end
   end
 end
