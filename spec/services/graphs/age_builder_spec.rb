1f6570727514849c64782d0062ad1317ca747435
blackchestnut <alexander.kalinichev@gmail.com>
2016-03-05 18:03:36 +0300

    Round 1 if age of issue less than 1 day #830

--- spec/services/graphs/age_builder_spec.rb
+++ spec/services/graphs/age_builder_spec.rb
@@ -14,24 +14,44 @@ describe Graphs::AgeBuilder do
     end
 
     context 'with issues' do
-      let(:issue) { stub_issue }
+      let(:issue) { stub_issue(created_at: created_at) }
       let(:issue_stat) do
-        build :issue_stat, column: board.columns.first, board: board
+        build(:issue_stat, column: board.columns.first, board: board)
       end
       let(:board_issue) { BoardIssue.new(issue, issue_stat) }
       let(:issues) { [board_issue] }
+      let(:created_at) { Time.current }
       before { allow(board_bag).to receive(:labels).and_return([]) }
       before { allow(board_bag).to receive(:collaborators).and_return([]) }
 
       it { is_expected.not_to be_empty }
-      its(:first) do
-        is_expected.to eq(
-          index: 1,
-          number: issue.number,
-          days: 0,
-          age: :n0,
-          issue: IssuePresenter.new(:issue, board_issue).to_hash(board_bag)
-        )
+
+      context 'age greate than 1' do
+        let(:created_at) { 1.day.ago }
+
+        its(:first) do
+          is_expected.to eq(
+            index: 1,
+            number: issue.number,
+            days: 1,
+            age: :n0,
+            issue: IssuePresenter.new(:issue, board_issue).to_hash(board_bag)
+          )
+        end
+      end
+
+      context 'age less than 1' do
+        let(:created_at) { 12.hours.ago }
+
+        its(:first) do
+          is_expected.to eq(
+            index: 1,
+            number: issue.number,
+            days: 0.5,
+            age: :n0,
+            issue: IssuePresenter.new(:issue, board_issue).to_hash(board_bag)
+          )
+        end
       end
     end
   end
