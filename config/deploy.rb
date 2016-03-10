abf1ae479b02e739850ab57b1526620c3035d173
blackchestnut <alexander.kalinichev@gmail.com>
2016-03-10 19:57:32 +0300

    Update ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'agileseason'
 set :repo_url, 'git@github.com:agileseason/agileseason.git'
 
 set :rbenv_type, :system
-set :rbenv_ruby, '2.2.3'
+set :rbenv_ruby, '2.3.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
 
