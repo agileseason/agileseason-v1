33008986fc69cee0e6e733790b0e68156f38a745
Daria <darya.office@gmail.com>
2016-03-14 21:33:34 +0300

    small fix

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,3 +1,4 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['clean','images', 'sass', 'watch']);
+gulp.task('default', ['images', 'sass', 'watch']);
+// gulp.task('default', ['clean','images', 'sass', 'watch']);
