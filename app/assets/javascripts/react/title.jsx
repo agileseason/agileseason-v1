9a5d799692ef3c6a3afd86508320aefa5d86cf26
blackchestnut <alexander.kalinichev@gmail.com>
2016-08-28 08:43:51 +0300

    Change due date style if time has passed #907

--- app/assets/javascripts/react/title.jsx
+++ app/assets/javascripts/react/title.jsx
@@ -129,16 +129,25 @@ var CurrentDueDate = React.createClass({
   getDate: function() {
     var res = '';
     if (this.props.data) {
-      var date = new Date(this.props.data);
+      var date = this.dueDate();
       var yearStr = (new Date()).getUTCFullYear() == date.getUTCFullYear() ? '' : '.' + date.getUTCFullYear();
       res = (date.getUTCDate()).pad() + '.' + (date.getUTCMonth() + 1).pad() + yearStr
         + ' ' + (date.getUTCHours()).pad() + ':' + (date.getUTCMinutes()).pad();
     }
     return res;
   },
+  dueDate: function() {
+    return new Date(this.props.data)
+  },
+  dueDateClasses: function() {
+    if (this.dueDate() < new Date()) {
+      return 'current-due-date passed'
+    }
+    return 'current-due-date'
+  },
   render: function() {
     if (this.props.data) {
-      return <div className='current-due-date' title='Due Date'>{this.getDate()}</div>;
+      return <div className={this.dueDateClasses()} title='Due Date'>{this.getDate()}</div>;
     } else {
       return <span />
     }
