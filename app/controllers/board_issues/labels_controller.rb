afe9b2d6bb18d760cbf8f954059c443213be62e3
blackchestnut <alexander.kalinichev@gmail.com>
2016-11-19 10:40:43 +0300

    Testing all formats for LabelsController

--- app/controllers/board_issues/labels_controller.rb
+++ app/controllers/board_issues/labels_controller.rb
@@ -5,7 +5,7 @@ module BoardIssues
     before_action :fetch_board_for_update
 
     def update
-      issue = github_api.update_issue(@board, number, issue_labels_params.to_h)
+      issue = github_api.update_issue(@board, number, labels_params.to_h)
       @board_bag.update_cache(issue)
       respond_to do |format|
         format.html { head :ok }
@@ -15,7 +15,7 @@ module BoardIssues
 
   private
 
-    def issue_labels_params
+    def labels_params
       # For variant when uncheck all labels
       params[:issue] ||= {}
       params[:issue][:labels] ||= []
